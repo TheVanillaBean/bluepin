a0a825c82a5a4ec1b680195fe17305043cb79acc
Joe <teambizmi@gmail.com>
2016-07-19 05:11:00 -0700

    Added Login VC, Custom Font,and Launch Screen.

---
+++ Bizmi/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Bizmi
+//
+//  Created by Alex on 7/19/16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ Bizmi/MaterialTextField.swift
@@ -0,0 +1,28 @@
+//
+//  MaterialTextField.swift
+//  Bizmi
+//
+//  Created by Alex on 7/19/16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+    }
+
+    //For Placholder
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    //For Editable Text
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+}
